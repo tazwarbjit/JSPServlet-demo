beac25fc401220c873edb133797f8eb8742dd5a3
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-08-27 16:41:43 +0600

    FIRST COMMIT

---
+++ src/main/java/com/tazwar/AddServlet.java
@@ -0,0 +1,20 @@
+package com.tazwar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		int k = i + j;
+//		System.out.println(k);
+		
+		PrintWriter out  = res.getWriter();
+		out.println("Result is " + k);
+	}
+}
